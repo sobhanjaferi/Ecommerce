869f4fee11bf4ddc74801cde89f24ff79b9c2e26
sobhan jafari <sobhanjafarii87@gmail.com>
2026-09-21 15:37:22 +0330

    [REFACTORE] header component

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -1,44 +1,10 @@
-'use client'
-
-import { NAVLIST } from '@/constants/navList.constant'
-import { ROUTES } from '@/constants/routes.constant'
-import Image from 'next/image'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
 import { type ReactElement } from 'react'
+import Navbar from './Navbar'
 
 export default function Header(): ReactElement {
-  const pathname = usePathname()
-
   return (
-    <header className='flex flex-col gap-5'>
-      <nav className='w-full p-4 rounded-b-xl flex justify-between gap-20 items-center bg-gray-100 mx-auto shadow-lg shadow-gray-400'>
-        <ul className='flex justify-between items-center gap-5'>
-          {NAVLIST.map((item) => (
-            <li
-              key={item.route}
-              className={item.route === pathname ? 'border-b' : 'text-gray-600'}
-            >
-              <Link
-                href={item.route}
-                className='cursor-pointer active:opacity-30'
-              >
-                {item.title}
-              </Link>
-            </li>
-          ))}
-        </ul>
-
-        <Link href={ROUTES.HOME}>
-          <Image
-            src='/images/logo.svg'
-            alt='logo'
-            width={1080}
-            height={1080}
-            className='w-8'
-          />
-        </Link>
-      </nav>
+    <header>
+      <Navbar />
     </header>
   )
 }
