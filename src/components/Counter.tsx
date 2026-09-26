3eefed5b20a63e42612fe0c299e21fb074017cba
sobhan jafari <sobhanjafarii87@gmail.com>
2026-09-26 20:44:12 +0330

    [ADD] counter component

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,25 +1,31 @@
 import type { ReactElement } from 'react'
 import IconButton from './IconButton'
-import { useCounter } from '@/hooks/Counter'
-import { Minus, PlusIcon } from 'lucide-react'
 
-export default function Counter(): ReactElement {
-  const { counter, handleDecrease, handleIncrease } = useCounter()
+import { Minus, PlusIcon } from 'lucide-react'
+import { CartStore } from '@/stores/cartStore'
+import { ProductItemType } from '@/types/productItem'
+
+export default function Counter({
+  product,
+}: {
+  product: ProductItemType
+}): ReactElement {
+  const { handleIncrease, handleDecrease, handleOrderQty } = CartStore()
 
   return (
     <div className='flex justify-between items-center gap-5'>
       <IconButton
         className='w-11 h-10.5 text-green-700 bg-green-100 font-bold text-2xl border-2 rounded-lg'
-        onClick={handleIncrease}
+        onClick={() => handleIncrease(product)}
       >
         <PlusIcon />
       </IconButton>
 
-      <span className='text-xl border-b'>{counter}</span>
+      <span className='text-xl border-b'>{handleOrderQty(product.id)}</span>
 
       <IconButton
         className='w-11 h-10.5 text-red-500 bg-red-100 font-bold text-2xl border-2 rounded-lg'
-        onClick={handleDecrease}
+        onClick={() => handleDecrease(product.id)}
       >
         <Minus />
       </IconButton>
